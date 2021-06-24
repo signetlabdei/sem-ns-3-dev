15b254194917024ea656fa135841ee21cbdc2642
akshitpatel01 <akshitpatel01@gmail.com>
2021-06-25 00:19:49 +0530

    Add base logging ns-3 script for SEM testing

---
+++ scratch/logging-ns3-script.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "ns3/core-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("Logger");
+
+int
+main (int argc, char *argv[])
+{
+
+  Simulator::Run ();
+  NS_LOG_DEBUG("Debug");
+  NS_LOG_INFO("Info");
+  NS_LOG_WARN("Warn");
+  NS_LOG_ERROR("Error");
+  NS_LOG_LOGIC("Logic");
+  // NS_LOG_FUNCTION("Function");
+  Simulator::Destroy ();
+}
