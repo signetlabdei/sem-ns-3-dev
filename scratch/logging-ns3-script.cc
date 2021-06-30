01d5f57d65546562474472cdd5bca492c5f8e341
akshitpatel01 <akshitpatel01@gmail.com>
2021-06-30 16:38:03 +0530

    Corrected coding style and added comments

--- scratch/logging-ns3-script.cc
+++ scratch/logging-ns3-script.cc
@@ -12,12 +12,13 @@ int
 main (int argc, char *argv[])
 {
 
+  // Siulator::Run() and Simulator::Destroy () are used to ensure that the NS_LOG macros print timestamp and context.
   Simulator::Run ();
-  NS_LOG_DEBUG("Debug");
-  NS_LOG_INFO("Info");
-  NS_LOG_WARN("Warn");
-  NS_LOG_ERROR("Error");
-  NS_LOG_LOGIC("Logic");
-  NS_LOG_FUNCTION("Function");
+  NS_LOG_DEBUG ("Debug");
+  NS_LOG_INFO ("Info");
+  NS_LOG_WARN ("Warn");
+  NS_LOG_ERROR ("Error");
+  NS_LOG_LOGIC ("Logic");
+  NS_LOG_FUNCTION ("Function");
   Simulator::Destroy ();
 }
